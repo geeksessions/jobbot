5b60ee93842e2b837245bdc6f2e1a1d9dcabaf86
Miguel Coquet <miguel@mcoquet.com>
2018-06-17 01:43:52 +0100

    adding couchdb connection

--- index.js
+++ index.js
@@ -23,15 +23,55 @@ const ws = require('ws')
 //   console.log(`server listening on ${fastify.server.address().port}`)
 // })
 
+const DB = new pouchdb("http://jobbot:jobz-for-tha-baes@localhost:5984/jobboard")
+
+DB.allDocs().then((data)=>{
+  console.log("mamammama",data)
+}).catch(console.error)
+
+function isMessage(message) {
+  if (message.type !== 'message') {
+    return false;
+  }
+
+  return true;
+}
+
+function isPinnedMessage(message) {
+  const isItAMessage = isMessage(message)
+  const isPinned = message.subtype === 'pinned_item'
+
+  return isItAMessage && isPinned;
+}
+
+function extractPost(message) {
+  const attachement = message.attachments[0]
+  let post = {
+    timestamp: attachement.ts,
+    author_id: attachement.author_id,
+    author_name: attachement.author_subname,
+    author_icon: attachement.author_icon,
+    raw: attachement.text,
+    mrkdwn_in: attachement.mrkdwn_in
+  }
+  
+  return post
+}
+
 bot.rtm.connect().then(({url}) => {
   const RTMWS = new ws(url);
   RTMWS.on('open', function open() {
     // RTMWS.send('something');
   });
   
-  RTMWS.on('message', (data) => {
-    const {type, user, text} = JSON.parse(data);
-    console.log(data);
+  RTMWS.on('message', (response) => {
+    data = JSON.parse(response)
+
+    if (isPinnedMessage(data)) {
+      const post = extractPost(data)
+      console.log(post)
+      DB.post(post).then(console.log).catch(console.error)
+    }
   });
 }).catch(console.error)
 
